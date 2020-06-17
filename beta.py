4984119766d2fd9937cf198803addc850e466079
GeekyAmit5 <amitmishra95820@gmail.com>
2020-06-17 20:45:19 +0530

    game under development

--- beta.py
+++ beta.py
@@ -5,6 +5,29 @@ import pygame
 import sys
 
 
+class button():
+    def __init__(self, rect, color=(0, 0, 0)):
+        self.rect = rect
+        self.color = color
+
+    def show(self):
+        pygame.draw.rect(win, self.color, self.rect)
+
+
+class text():
+    def __init__(self, msg, x, y, color=(255, 255, 255), size=40, font=None):
+        self.msg = msg
+        self.x = x
+        self.y = y
+        self.color = color
+        self.size = size
+        self.font = font
+
+    def show(self):
+        win.blit(pygame.font.Font(self.font, self.size).render(self.msg, True,
+                                                               self.color), (self.x, self.y))
+
+
 def isPossible(x, y, n):
     for i in range(9):
         if grid[x][i] == n or grid[i][y] == n:
@@ -26,8 +49,7 @@ def solve():
                         grid[i][j] = n
                         pygame.draw.rect(
                             win, green, (22+j*62, 22+i*62, 60, 60))
-                        score = font.render(str(n), True, black)
-                        win.blit(score, (43+j*62, 40+i*62))
+                        text(str(n), 43+j*62, 40+i*62, black).show()
                         pygame.display.update()
                         solve()
                         grid[i][j] = 0
@@ -43,7 +65,7 @@ def solve():
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                game(0)
+                game(1)
 
 
 def drawGrid(color):
@@ -75,17 +97,19 @@ def load_sudoku():
     for i in range(9):
         for j in range(9):
             if grid[i][j]:
-                score = font.render(str(grid[i][j]), True, black)
-                win.blit(score, (43+j*62, 40+i*62))
+                text(str(grid[i][j]), 43+j*62, 40+i*62, black).show()
 
 
 def game(level):
+    global grid
     win.blit(bg, (0, 0))
+    for i in range(9):
+        for j in range(9):
+            grid[i][j] = sudoku[level][i][j]
     load_sudoku()
     for i in range(4):
-        pygame.draw.rect(win, black, rect[i])
-        win.blit(font.render(gamemenu[i], True,
-                             white), (rect[i].x+20, rect[i].y+15))
+        button(rect[i]).show()
+        text(gamemenu[i], rect[i].x+20, rect[i].y+15).show()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -95,7 +119,7 @@ def game(level):
                 for i in range(4):
                     if rect[i].collidepoint(pygame.mouse.get_pos()):
                         if not i:
-                            reset()
+                            game(level)
                         elif i == 1:
                             solve()
                         elif i == 2:
@@ -109,10 +133,34 @@ def game(level):
 def options():
     global sound, time_limit
     win.blit(bg, (0, 0))
-    for i in range(4):
-        pygame.draw.rect(win, black, rect[i])
-        win.blit(font.render(optionsmenu[i], True,
-                             white), (rect[i].x+20, rect[i].y+15))
+
+    button(rect[0]).show()
+    text("About", rect[0].x+20, rect[0].y+15).show()
+    button(rect[3]).show()
+    text("Back", rect[3].x+20, rect[3].y+15).show()
+
+    b1 = button(rect[1])
+    t1 = text("Time Limit On", rect[1].x+20, rect[1].y+15)
+    if time_limit:
+        b1.color = green
+        t1.msg = "Time Limit On"
+    else:
+        b1.color = red
+        t1.msg = "Time Limit Off"
+    b1.show()
+    t1.show()
+
+    b2 = button(rect[2])
+    t2 = text("Sound On", rect[2].x+20, rect[2].y+15)
+    if sound:
+        b2.color = green
+        t2.msg = "Sound On"
+    else:
+        b2.color = red
+        t2.msg = "Sound Off"
+    b2.show()
+    t2.show()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -124,28 +172,29 @@ def options():
                         if not i:
                             pass
                         elif i == 1:
-                            if time_limit:
-                                time_limit = False
-                                pygame.draw.rect(win, red, rect[i])
-                                optionsmenu[1] = "Time Limit Off"
-                            else:
+                            if not time_limit:
+                                b1.color = green
+                                t1.msg = "Time Limit On"
                                 time_limit = True
-                                pygame.draw.rect(win, green, rect[i])
-                                optionsmenu[1] = "Time Limit On"
-                        elif i == 2:
-                            if sound:
-                                sound = False
-                                pygame.draw.rect(win, red, rect[i])
-                                optionsmenu[2] = "Sound Off"
                             else:
+                                b1.color = red
+                                t1.msg = "Time Limit Off"
+                                time_limit = False
+                        elif i == 2:
+                            if not sound:
+                                b2.color = green
+                                t2.msg = "Sound On"
                                 sound = True
-                                pygame.draw.rect(win, green, rect[i])
-                                optionsmenu[2] = "Sound On"
+                            else:
+                                b2.color = red
+                                t2.msg = "Sound Off"
+                                sound = False
                         elif i == 3:
                             main()
-        for i in range(4):
-            win.blit(font.render(optionsmenu[i], True,
-                                 white), (rect[i].x+20, rect[i].y+15))
+                        b1.show()
+                        t1.show()
+                        b2.show()
+                        t2.show()
         Clock.tick(fps)
         pygame.display.update()
 
@@ -153,9 +202,8 @@ def options():
 def difficulty():
     win.blit(bg, (0, 0))
     for i in range(4):
-        pygame.draw.rect(win, black, rect[i])
-        win.blit(font.render(difficultymenu[i], True,
-                             white), (rect[i].x+20, rect[i].y+15))
+        button(rect[i]).show()
+        text(difficultymenu[i], rect[i].x+20, rect[i].y+15).show()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -175,9 +223,8 @@ def difficulty():
 def main():
     win.blit(bg, (0, 0))
     for i in range(4):
-        pygame.draw.rect(win, black, rect[i])
-        win.blit(font.render(mainmenu[i], True,
-                             white), (rect[i].x+20, rect[i].y+15))
+        button(rect[i]).show()
+        text(mainmenu[i], rect[i].x+20, rect[i].y+15).show()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -205,38 +252,51 @@ pygame.display.set_caption("Sudoku")
 # icon = pygame.image.load("assets/images/icon.ico")
 # pygame.display.set_icon(icon)
 bg = pygame.image.load("assets/images/bg.jpg")
-
 rect = []
 for i in range(4):
     rect.append(pygame.Rect(625, 240+70*i, 300, 60))
 mainmenu = ["Continue", "New Game", "Options", "Exit"]
 difficultymenu = ["Easy", "Medium", "Hard", "Back"]
-optionsmenu = ["About", "Time Limit off", "Sounds on", "Back"]
 gamemenu = ["Reset", "Solution", "New Game", "Main Menu"]
-font = pygame.font.Font(None, 40)
 Clock = pygame.time.Clock()
-black = (0, 0, 0)
 white = (255, 255, 255)
 green = (51, 204, 89)
 red = (250, 51, 51)
+black = (0, 0, 0)
+time_limit = True
+sound = True
 fps = 10
 
 
 total = 0
-time_limit = True
-sound = True
-grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
-        [5, 2, 0, 0, 0, 0, 0, 0, 0],
-        [0, 8, 7, 0, 0, 0, 0, 3, 1],
-        [0, 0, 3, 0, 1, 0, 0, 8, 0],
-        [9, 0, 0, 8, 6, 3, 0, 0, 5],
-        [0, 5, 0, 0, 9, 0, 6, 0, 0],
-        [1, 3, 0, 0, 0, 0, 2, 5, 0],
-        [0, 0, 0, 0, 0, 0, 0, 7, 4],
-        [0, 0, 5, 2, 0, 6, 3, 0, 0]]
+s1 = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
+      [5, 2, 0, 0, 0, 0, 0, 0, 0],
+      [0, 8, 7, 0, 0, 0, 0, 3, 1],
+      [0, 0, 3, 0, 1, 0, 0, 8, 0],
+      [9, 0, 0, 8, 6, 3, 0, 0, 5],
+      [0, 5, 0, 0, 9, 0, 6, 0, 0],
+      [1, 3, 0, 0, 0, 0, 2, 5, 0],
+      [0, 0, 0, 0, 0, 0, 0, 7, 4],
+      [0, 0, 5, 2, 0, 6, 3, 0, 0]]
+
+s2 = [[2, 0, 0, 0, 0, 0, 5, 0, 0],
+      [8, 0, 0, 0, 6, 0, 2, 0, 0],
+      [0, 0, 0, 0, 0, 8, 0, 7, 0],
+      [0, 0, 9, 0, 0, 7, 0, 0, 0],
+      [0, 0, 0, 9, 0, 0, 8, 0, 0],
+      [0, 0, 1, 0, 0, 5, 0, 0, 7],
+      [0, 0, 0, 0, 1, 0, 0, 0, 6],
+      [0, 2, 0, 7, 0, 0, 0, 4, 0],
+      [0, 5, 0, 6, 0, 0, 0, 1, 0]]
+
+
+sudoku = [s1, s2]
+grid = [[0 for x in range(9)]for y in range(9)]
 
 
 main()
+
+
 
 
 # def generateSudoku():
