46c7c57933b860abd3e0dd089c91bc8b187f9998
Amit Mishra <63869360+GeekyAmit5@users.noreply.github.com>
2020-08-06 16:54:54 +0530

    backgroud color update

--- test.py
+++ test.py
@@ -2,121 +2,10 @@ import pygame
 import sys
 
 
-class text():
-    def __init__(self, msg, x, y, color=(255, 255, 255), size=40, font=None):
-        self.msg = msg
-        self.x = x
-        self.y = y
-        self.color = color
-        self.size = size
-        self.font = font
-
-    def show(self):
-        win.blit(pygame.font.Font(self.font, self.size).render(self.msg, True,
-                                                               self.color), (self.x, self.y))
-
-
-def drawGrid(color):
-    s = 20
-    e = 580
-    d = 62
-    n = 10
-    w = 5
-    for i in range(n):
-        pygame.draw.line(win, color, (s+d*i, s), (s+d*i, e), w)
-    for i in range(n):
-        pygame.draw.line(win, color, (s, s+d*i), (e, s+d*i), w)
-
-
-def load_sudoku():
-    pygame.draw.rect(win, white, (20, 20, 560, 560))
-    drawGrid(black)
-    for i in range(9):
-        for j in range(9):
-            if grid[i][j]:
-                text(str(grid[i][j]), 43+j*62, 40+i*62, black).show()
-    pygame.display.update()
-
-
-def main():
-    global grid
-    win.fill(black)
-    drawGrid(white)
-    prevx, prevy = -1, -1
-    xcord, ycord = -1, -1
-    load_sudoku()
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                mx, my = pygame.mouse.get_pos()
-                if rect.collidepoint((mx, my)):
-                    active = True
-                else:
-                    active = False
-                if active:
-                    x = (mx-20)//62
-                    y = (my-20)//62
-                    xcord = 40+x*62
-                    ycord = 40+y*62
-                    print(x, y)
-                    print(xcord, ycord)
-            if event.type == pygame.KEYDOWN:
-                if active:
-                    if event.key == pygame.K_BACKSPACE:
-                        grid[x][y] = 0
-                        pygame.draw.rect(
-                            win, black, (xcord-20, ycord-20, 40, 40))
-                    else:
-                        if grid[x][y] == 0:
-                            grid[x][y] = int(event.unicode)
-                            win.blit(font.render(str(grid[x][y]), True, black),
-                                     (xcord, ycord))
-        Clock.tick(fps)
-        pygame.display.update()
-
-
-pygame.init()
-win = pygame.display.set_mode((1000, 600))
-pygame.display.set_caption("Testing")
-Clock = pygame.time.Clock()
-white = (255, 255, 255)
-green = (51, 204, 89)
-red = (250, 51, 51)
-black = (0, 0, 0)
-fps = 10
-font = pygame.font.Font(None, 50)
-rect = pygame.Rect(20, 20, 560, 560)
-active = False
-grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
-        [5, 2, 0, 0, 0, 0, 0, 0, 0],
-        [0, 8, 7, 0, 0, 0, 0, 3, 1],
-        [0, 0, 3, 0, 1, 0, 0, 8, 0],
-        [9, 0, 0, 8, 6, 3, 0, 0, 5],
-        [0, 5, 0, 0, 9, 0, 6, 0, 0],
-        [1, 3, 0, 0, 0, 0, 2, 5, 0],
-        [0, 0, 0, 0, 0, 0, 0, 7, 4],
-        [0, 0, 5, 2, 0, 6, 3, 0, 0]]
-
-
-main()
-
-
-
-
-
-
-
-import pygame
-import sys
-
-
 pygame.init()
 win_width = 1000
 win_height = 600
-win = pygame.display.set_mode((win_width,win_height))
+win = pygame.display.set_mode((win_width, win_height))
 pygame.display.set_caption("Sudoku")
 Clock = pygame.time.Clock()
 fps = 60
@@ -126,10 +15,10 @@ green = (51, 204, 89)
 red = (250, 51, 51)
 
 
-#creating cell class that represent every square in sudoku
+# creating cell class that represent every square in sudoku
 class cell:
-    def __init__(self,(i,j),rect,color,border,border_color,num,num_color):
-        self.id = (i,j)
+    def __init__(self, id, rect, color, border, border_color, num, num_color):
+        self.id = id
         self.rect = rect
         self.color = color
         self.border = border
@@ -139,15 +28,15 @@ class cell:
         self.isActive = False
         self.isPermanant = self.num != 0
 
-
     def show(self):
-        pygame.draw.rect(win,self.color,self.rect)
-        pygame.draw.rect(win,self.border_color,self.rect,self.border)
+        pygame.draw.rect(win, self.color, self.rect)
+        pygame.draw.rect(win, self.border_color, self.rect, self.border)
         if self.num:
-            win.blit(num_font.render(str(self.num),True,self.num_color), (self.rect.x+20,self.rect.y+15))
+            win.blit(num_font.render(str(self.num), True,
+                                     self.num_color), (self.rect.x+20, self.rect.y+15))
 
 
-#grid attributes
+# grid attributes
 grid_side_margin = 100
 grid_top_margin = 30
 cell_size = 60
@@ -156,7 +45,7 @@ cell_border_color = black
 cell_color = white
 cell_num_color = black
 cell_num = 0
-num_font = pygame.font.SysFont(None,40)
+num_font = pygame.font.SysFont(None, 40)
 grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
         [5, 2, 0, 0, 0, 0, 0, 0, 0],
         [0, 8, 7, 0, 0, 0, 0, 3, 1],
@@ -171,18 +60,22 @@ grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
 cells = []
 for i in range(9):
     for j in range(9):
-        object = cell((i,j),pygame.Rect(grid_side_margin+j*cell_size,grid_top_margin+i*cell_size,cell_size,cell_size),cell_color,cell_border,cell_border_color,grid[i][j],cell_num_color)
+        object = cell((i, j), pygame.Rect(grid_side_margin+j*cell_size, grid_top_margin+i*cell_size,
+                                          cell_size, cell_size), cell_color, cell_border, cell_border_color, grid[i][j], cell_num_color)
         cells.append(object)
 
 
 def isPossible(id, n):
-    for i in range(9):
-        if cell.id[0]
-        if grid[x][i] == n or grid[i][y] == n:
-            return False
-    for i in range(3):
-        for j in range(3):
-            if grid[(x//3)*3+i][(y//3)*3+j] == n:
+    x = id[0]//3
+    y = id[1]//3
+    for cell in cells:
+        if cell.id == id:
+            continue
+        elif (cell.id[0] == id[0] or cell.id[1] == id[1]):
+            if cell.num == n:
+                return False
+        elif cell.id[0]//3 == x and cell.id[1]//3 == y:
+            if cell.num == n:
                 return False
     return True
 
@@ -206,7 +99,10 @@ def main():
                             cell.num = 0
                         else:
                             cell.num = int(event.unicode)
-                            
+                            if isPossible(cell.id, cell.num):
+                                cell.color = green
+                            else:
+                                cell.color = red
 
         win.fill(white)
         for cell in cells:
